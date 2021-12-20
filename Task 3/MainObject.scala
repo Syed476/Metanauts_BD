615a2c9ac41effd289a0b25c70ba70c3f21c9c10
bbk-pij-77 <sali33@mail.bbk.ac.uk>
2021-12-20 12:26:30 +0000

    MainObject.scala uploaded

--- Task 3/MainObject.scala
+++ Task 3/MainObject.scala
@@ -1,20 +1,27 @@
 package com.test.scala
 
-import com.test.scala.Scala_Occurrences.elemnt_occurrences
 
 import scala.io.Source
 
 object MainObject{
+  def element_occurrences[A](list1:List[A]):Map[A, Int] = {
+    list1.groupBy(el => el).map(e => (e._1, e._2.length))
+  }
   def main(args:Array[String]){
 
-
+    var finalList = List.empty[String]
     val filename = "Shakespeare.txt"
     val fileSource = Source.fromFile(filename)
 
     for(line<-fileSource.getLines){
-      println(line)
-    }
+      //println(line)
+      var str : String = line
+      val trimmedList: List[String] = str.split(" ").map(_.trim).toList
+      finalList = List.concat(finalList, trimmedList)
 
+    }
+    println (finalList)
+    println(element_occurrences(finalList))
     fileSource.close()
   }
 }
